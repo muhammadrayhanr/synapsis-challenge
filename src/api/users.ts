02f21527b120130c6666bc64deb15cc5a285ffe9
muhammadrayhanr <mrayhanr4869@gmail.com>
2025-02-22 13:06:17 +0700

    feat: integrated edit user with API

--- src/api/users.ts
+++ src/api/users.ts
@@ -8,3 +8,19 @@ export const getUsers = async () => {
   });
   return response.data;
 };
+
+export const editUsers = async (
+  data: UserProps,
+  id: number
+): Promise<UserProps> => {
+  const response = await axios.put(
+    `https://gorest.co.in/public/v2/users/${id}`,
+    data,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_GOREST_TOKEN}`,
+      },
+    }
+  );
+  return response.data;
+};
