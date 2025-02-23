99b15a2ed7c3f4692cd333c7e664ea31cfe0ed0d
muhammadrayhanr <mrayhanr4869@gmail.com>
2025-02-23 17:06:24 +0700

    refactor: changed folder structure theme config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ConfigProvider } from 'antd';
 import type { AppProps } from 'next/app';
-import theme from '@/pages/theme/themeConfig';
+import theme from '@/theme/themeConfig';
 import { QueryClientProvider } from '@tanstack/react-query';
 import queryClient from '@/config/providers/queryClient';
 import '@/styles/globals.css';
